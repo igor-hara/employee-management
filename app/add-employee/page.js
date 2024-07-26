7c2d5b37f67463d737d95e54506d260ac58e5de5
igor-hara <munkieryder@gmail.com>
2024-07-26 22:00:49 +0200

    fix/form css, larger input text

--- app/add-employee/page.js
+++ app/add-employee/page.js
@@ -3,8 +3,6 @@ import EmployeeForm from '@/app/components/EmployeeForm';
 const AddEmployeePage = () => {
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
-      <h1 className='text-3xl font-bold'>Add EmployeePage</h1>
-
       <EmployeeForm />
     </main>
   );
